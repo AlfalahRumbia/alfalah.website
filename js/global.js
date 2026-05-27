239cbf88deea199b39f21ccb046d4d7b89f9e1d9
TohaRizalahmad <toharizalahmad24@gmail.com>
2026-05-27 21:53:08 +0700

    update

--- js/global.js
+++ js/global.js
@@ -1,3 +1,18 @@
+// Smart back navigation: go back if there's history, otherwise go to index.html
+function smartBack() {
+  if (document.referrer && document.referrer !== '') {
+    history.back();
+  } else {
+    // Detect if we're in /pages/ subfolder or at root
+    const path = window.location.pathname;
+    if (path.includes('/pages/')) {
+      window.location.href = '../index.html';
+    } else {
+      window.location.href = 'index.html';
+    }
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   // Global fade-up animation using IntersectionObserver
   const obs = new IntersectionObserver(entries => {
